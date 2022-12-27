34c263141b42de014fee355fa498b9b3acb7ca5e
6eJapycc <dsenazatskij@gmail.com>
2022-12-27 19:07:08 +0300

    .

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Exercises {
+import java.util.Scanner;
+public class Exercises {
+    public static void main(String[] args) {
+
+        /*1. Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+        String reverse = new StringBuffer(number).reverse().toString();
+        System.out.println(reverse);
+
+        2. Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int max1 = 9, min1 = 0;
+        int max = 0, min = 9;
+
+        for(; number > 0; number /= 10) {
+            max1 = number % 10;
+            min1 = number % 10;
+            if(max1 > max) {
+                max = max1;
+            }
+            if(min1 < min) {
+                min = min1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+
+        3. Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int x, y, t = 0;
+        for(int i = number; i > 0; i /= 10) {
+            y = i % 10;
+            for(int j = i / 10; j > 0; j /= 10) {
+                x = j % 10;
+                if(x == y){
+                    t++;
+                }
+            }
+        }
+        if(t == 0){
+            System.out.println(true);
+        } else {
+            System.out.println("Есть цифры дубликаты");
+        } */
+    }
 }
